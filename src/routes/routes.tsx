73c44a3f25b50b81c549d5c877b5685f12b21939
mdasif8393 <md.asif.8393@gmail.com>
2025-01-09 11:45:49 +0600

    26-6 A centralised array for routes and sidebar items.

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,54 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateAdmin from "../pages/admin/CreateAdmin";
-import CreateFaculty from "../pages/admin/CreateFaculty";
-import CreateStudent from "../pages/admin/CreateStudent";
+import { adminPaths } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        index: true,
-        element: <About />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
   },
   {
     path: "/admin",
     element: <App />,
-    children: [
-      {
-        path: "dashboard",
-        element: <AdminDashboard />,
-      },
-      {
-        path: "create-student",
-        element: <CreateStudent />,
-      },
-      {
-        path: "create-admin",
-        element: <CreateAdmin />,
-      },
-      {
-        path: "create-faculty",
-        element: <CreateFaculty />,
-      },
-    ],
+    children: adminPaths,
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: adminPaths,
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: adminPaths,
   },
   {
     path: "/login",
