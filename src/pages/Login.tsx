908a4061d769ddf8c410b3f46530c6e02b1a2c61
mdasif8393 <md.asif.8393@gmail.com>
2025-01-11 16:19:00 +0600

    27-5 Basic Login form and useLoginMutaion

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,40 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0002",
+      password: "admin123",
+    },
+  });
+
+  const [login, { data, error }] = useLoginMutation();
+
+  console.log("data==>", data);
+  console.log("error==>", error);
+
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.userId,
+      Password: data.password,
+    };
+    login(userInfo);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
   );
 };
 
