7f4b3889398e303ce272e97e6ad2990e2b61bb68
mdasif8393 <md.asif.8393@gmail.com>
2025-01-21 18:59:44 +0600

    31-9 Add Faculties

--- src/pages/admin/coursemanagement/Courses.tsx
+++ src/pages/admin/coursemanagement/Courses.tsx
@@ -1,8 +1,101 @@
+import { Button, Modal, Table } from "antd";
+import { useState } from "react";
+import PHForm from "../../../components/form/PHForm";
+import PHSelect from "../../../components/form/PHSelect";
+import {
+  useAddFacultiesMutation,
+  useGetAllCoursesQuery,
+} from "../../../redux/features/admin/courseManagement.api";
+import { useGetAllHumanFacultiesQuery } from "../../../redux/features/admin/userManagement.api";
+
 const Courses = () => {
+  // const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+
+  const { data: courses, isFetching } = useGetAllCoursesQuery(undefined);
+
+  const tableData = courses?.data?.map(({ _id, title, prefix, code }) => ({
+    key: _id,
+    title,
+    code: `${prefix}${code}`,
+  }));
+
+  const columns = [
+    {
+      title: "Title",
+      key: "title",
+      dataIndex: "title",
+    },
+    {
+      title: "Code",
+      key: "code",
+      dataIndex: "code",
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: (item) => {
+        return <AddFacultyModal facultyInfo={item} />;
+      },
+    },
+  ];
+
   return (
-    <div>
-      <h1>Courses Courses</h1>
-    </div>
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      // onChange={onChange}
+    />
+  );
+};
+
+const AddFacultyModal = ({ facultyInfo }) => {
+  const { data: facultiesData } = useGetAllHumanFacultiesQuery(undefined);
+  const [addFaculties] = useAddFacultiesMutation();
+
+  const facultiesOption = facultiesData?.data.map((facultyInfo) => ({
+    value: facultyInfo._id,
+    label: facultyInfo.fullName,
+  }));
+
+  const handleSubmit = (data) => {
+    const facultyData = {
+      courseId: facultyInfo.key,
+      data: data,
+    };
+    addFaculties(facultyData);
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <>
+      <Button onClick={showModal}>Add Faculty</Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <PHForm onSubmit={handleSubmit}>
+          <PHSelect
+            options={facultiesOption}
+            name="faculties"
+            label="Faculty"
+            mode="multiple"
+          />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Modal>
+      ;
+    </>
   );
 };
 
